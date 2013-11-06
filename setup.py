4ea934994938fe117ab395f282f50c6c0b7956db
lh_intern <lh_intern@douban.com>
2013-11-06 15:33:32 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,46 @@
 from setuptools import setup
 
-setup(name='ellen',
-      version='0.0.1',
-      description='Ellen is a wrapper of pygit2 and git command.',
-      author='xutao',
-      author_email='xutao@douban.com',
-      license='BSD',
-      packages=['ellen'],
-      zip_safe=False,
-      test_suite='nose.collector',
-      tests_require=['nose'],)
+from ellen import __version__
+
+# http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+
+with open('README.md') as long_description_file:
+    long_description = long_description_file.read()
+
+dependency_links = [
+    'git+https://github.com/douban/pygit2.git@0674872bfe99c9fcf3dfca5a7ac8ecfc2af8bff9#egg=pygit2'
+]
+
+install_requires = [
+    'pygit2',
+    'python-magic==0.4.3',
+    'gevent',
+]
+
+tests_require = [
+    'nose',
+] + install_requires
+
+
+setup(
+    name='ellen',
+    version=__version__,
+    description='Ellen is a wrapper of pygit2 and git command.',
+    long_description=long_description,
+    author='xutao',
+    author_email='xutao@douban.com',
+    url='https://github.com/douban/ellen',
+    license='BSD',
+    packages=['ellen'],
+    keywords=['git', 'pygit2'],
+    zip_safe=False,
+    test_suite='nose.collector',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    dependency_links=dependency_links,
+)
