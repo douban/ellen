a7caba39f6b4ade87d8e09b8bea7592883727c05
lh_intern <lh_intern@douban.com>
2013-11-06 15:33:32 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
+from ellen import __version__
+
 setup(name='ellen',
-      version='0.0.1',
+      version=__version__,
       description='Ellen is a wrapper of pygit2 and git command.',
       author='xutao',
       author_email='xutao@douban.com',
+      url='http://code.dapps.douban.com/xutao/ellen',
       license='BSD',
       packages=['ellen'],
+      keywords=['git', 'pygit2'],
       zip_safe=False,
       test_suite='nose.collector',
       tests_require=['nose'],)
