944b3b91266110186cfa90dd8236be7f0fcd691e
XTao <xutao881001@gmail.com>
2013-12-14 13:40:09 +0800

    Add env to push.

--- ellen/git/push.py
+++ ellen/git/push.py
@@ -4,7 +4,7 @@
 from ellen.utils.process import call
 
 
-def push(repository, remote, ref):
+def push(repository, remote, ref, _env=None):
     cmd = ['push', remote, ref]
-    errcode = call(repository, cmd)
+    errcode = call(repository, cmd, env=_env)
     return errcode
