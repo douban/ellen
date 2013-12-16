8ba82fe5dcdf580fabdbf4df06c3d59bb5fd2e50
lh_intern <tclh123@gmail.com>
2013-12-16 20:06:37 +0800

    add Process

--- ellen/utils/process.py
+++ ellen/utils/process.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+# TODO: remove this?
 def _shlex_split(cmd):
     if isinstance(cmd, unicode):
         return [c.decode("utf-8") for c in shlex.split(cmd.encode("utf-8"))]
@@ -35,12 +36,53 @@ def _call(cmd, env=None):
     err = str(err)
 
     result = {}
-
     result['returncode'] = process.returncode
     result['stdout'] = out
     result['stderr'] = err
     result['fullcmd'] = " ".join(cmd)
     return result
+
+
+class Process(object):
+    def __init__(self, cmds=None):
+        self.cmds = cmds or []
+
+    def __getattr__(self, name):
+        return self.bake(name)
+
+    def __call__(self, *a, **kw):
+        proc = self.bake()
+        env = kw.pop('env', {})
+
+        for k, v in kw.iteritems():
+            if len(k) == 1:
+                k = '-' + k
+            else:
+                k = '--' + k
+            if isinstance(v, bool) and v is True:
+                proc.cmds.append(k)
+            else:
+                proc.cmds.append(k)
+                proc.cmds.append(v)
+
+        for p in a:
+            proc.cmds.append(p)
+
+        return proc.call(env=env)
+
+    def bake(self, cmd=None):
+        cmds = list(self.cmds)
+        if cmd:
+            cmds.append(cmd)
+        return Process(cmds)
+
+    def call(self, cmdstr='', env=None):
+        extra_cmds = _shlex_split(cmdstr)
+        return _call(self.cmds + extra_cmds, env=env)
+
+
+process = Process()
+git = process.bake('git')
 
 
 # TODO: remove repository.
