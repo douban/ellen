c095655713965042187a8d2e3e87a87a5f8ca30b
lh_intern <tclh123@gmail.com>
2013-12-17 02:05:42 +0800

    add ellen.utils.temp_repo.commit_something

--- ellen/utils/temp_repo.py
+++ ellen/utils/temp_repo.py
@@ -24,3 +24,24 @@ def create_temp_repo(path, is_bare=True):
                      data=data)
 
     return repo
+
+
+def commit_something(path, branch='master', file_name='something'):
+    repo = Jagare(path)
+
+    data = [(os.path.join(path, file_name),
+             """test_content
+             test_content
+             test_content
+             """,
+             'insert')]
+
+    repo.commit_file(branch=branch,
+                     parent=branch,
+                     author_name='testuser',
+                     author_email='testuser@xxx.com',
+                     message='commit something..',
+                     reflog='commit one file',
+                     data=data)
+
+    return repo
