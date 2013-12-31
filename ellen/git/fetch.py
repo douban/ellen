f010c7828186c55e0dbf1995f4998104653dee69
XTao <xutao881001@gmail.com>
2014-01-01 00:57:47 +0800

    Add fetch_.

--- ellen/git/fetch.py
+++ ellen/git/fetch.py
@@ -4,7 +4,7 @@
 from ellen.utils.process import git_with_repo
 
 
-def fetch(repository):
+def fetch_repository(repository, env=None):
     git = git_with_repo(repository)
-    git.fetch()
+    git.fetch(q=True, env=env)
 
