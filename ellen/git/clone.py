dc8caca4708704a3daf4217544f8bd9ed1c649c3
XTao <xutao881001@gmail.com>
2014-06-11 17:03:26 +0800

    Add shared clone.

--- ellen/git/clone.py
+++ ellen/git/clone.py
@@ -5,7 +5,7 @@ from ellen.utils.process import git, git_with_repo
 
 
 def clone_repository(url, path, bare=None, checkout_branch=None, mirror=None,
-                     env=None):
+                     env=None, shared=None):
     """git clone command
     NOTE: 因为`git clone`在本机上直接做硬链接,速度比pygit2.clone_repository快
     """
@@ -13,7 +13,8 @@ def clone_repository(url, path, bare=None, checkout_branch=None, mirror=None,
                      b=checkout_branch,
                      bare=bare,
                      mirror=mirror,
-                     env=env)
+                     env=env,
+                     shared=shared)
 
 
 def update_server_info(repository):
