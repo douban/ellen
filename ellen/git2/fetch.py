fd2c18806a0e734764b778ac347fa7bb132764c9
XTao <xutao881001@gmail.com>
2014-09-08 15:12:58 +0800

    Using pygit2 fetch.

--- ellen/git2/fetch.py
+++ ellen/git2/fetch.py
@@ -3,6 +3,16 @@
 from ellen.process import git_with_repo
 
 
-def fetch(repository, *w, **kw):
+def fetch2(repository, *w, **kw):
     git = git_with_repo(repository)
     return git.fetch(*w, **kw)
+
+
+def fetch(repository, name):
+    remotes = {remote.name: remote
+               for remote in self.repository.remotes}
+    if name:
+        remotes[name].fetch()
+        return None
+    for key in remotes:
+        remotes[key].fetch()
